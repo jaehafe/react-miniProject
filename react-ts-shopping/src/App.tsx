39f139ba30048abb24f9f0ef62e72aa22c51d717
Jaeha Lee <wogkdkrm112@gmail.com>
2022-11-23 10:43:37 +0900

    react-ts shopping cart react-query styled-components

---
+++ react-ts-shopping/src/App.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+// components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// styles
+import { Wrapper, StyledButton } from './App.styles';
+import Item from './Item/Item';
+import Cart from './Cart/Cart';
+
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  const res = await fetch('http://fakestoreapi.com/products');
+  const data = res.json();
+  return data;
+};
+
+const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
+  const {
+    data: items,
+    isLoading,
+    error,
+  } = useQuery<CartItemType[]>(['products'], getProducts);
+
+  const getTotalItems = (items: CartItemType[]): number => {
+    return items.reduce((acc: number, item) => acc + item.amount, 0);
+  };
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // 1. 아이템이 카트에 이미 있는지?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // 아이템이 처음 추가 되는 경우
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((acc, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return acc;
+          return [...acc, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...acc, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge
+          overlap="rectangular"
+          badgeContent={getTotalItems(cartItems)}
+          color="error"
+        >
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {items?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+};
+
+export default App;
